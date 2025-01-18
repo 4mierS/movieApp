c19431c465fd035c072a0dd40b4627a5383b108d
Amier Shraideh <amiershr@gmail.com>
2025-01-18 12:00:08 +0100

    better readability

--- src/App.tsx
+++ src/App.tsx
@@ -17,17 +17,13 @@ import {
 import { IonReactRouter } from "@ionic/react-router"
 import { ListProvider } from "./components/Lists"
 import { eye, home, library, shuffle } from "ionicons/icons"
-import React from "react"
 
-/* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css"
 
-/* Basic CSS for apps built with Ionic */
 import "@ionic/react/css/normalize.css"
 import "@ionic/react/css/structure.css"
 import "@ionic/react/css/typography.css"
 
-/* Optional CSS utils that can be commented out */
 import "@ionic/react/css/padding.css"
 import "@ionic/react/css/float-elements.css"
 import "@ionic/react/css/text-alignment.css"
@@ -42,8 +38,8 @@ import "@ionic/react/css/display.css"
  * https://ionicframework.com/docs/theming/dark-mode
  */
 
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
+//import '@ionic/react/css/palettes/dark.always.css';
+//import '@ionic/react/css/palettes/dark.class.css';
 import "@ionic/react/css/palettes/dark.system.css"
 
 /* Theme variables */
@@ -53,6 +49,7 @@ import { useTranslation } from "react-i18next"
 import SplashScreen from "./components/SplashScreen"
 
 setupIonicReact()
+
 const App: React.FC = () => {
   const { t } = useTranslation()
 
@@ -109,7 +106,5 @@ const App: React.FC = () => {
     </IonApp>
   )
 }
-
-
 
 export default App
